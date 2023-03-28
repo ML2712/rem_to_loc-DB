25fb4bbdd7a8a4bf84ff8c1c0920e47d2ca33ecb
Manav Lakhanpal <67679350+ML2712@users.noreply.github.com>
2023-03-28 16:50:03 +0530

    Add files via upload

--- try_connect.py
+++ try_connect.py
@@ -1,16 +1,49 @@
 import pyodbc
-import pandas as pd
+from urllib.error import URLError
+import urllib.request
+import time
 
-conn = pyodbc.connect('Driver={SQL Server};'
-                      'Server=MININT-BDIHI4H;'
-                      'Database=NORTHWND;'
-                      'Trusted_Connection=yes;')
 
-cursor = conn.cursor()
-cursor.execute('SELECT TOP(5) CompanyId FROM dbo.Customers')
+# Check internet connection
+def is_internet_available():
+    try:
+        urllib.request.urlopen('https://www.google.com/', timeout=1)
+        return True
+    except URLError as err:
+        return False
 
-for i in cursor:
-    print(i)
 
-cursor.close()
-conn.close()
+# Switch between online and offline databases based on internet availability
+def switch_database():
+    while True:
+        if is_internet_available():
+            print('Internet connection available, switching to online database...')
+            print('Connected to online database.')
+            # time.sleep(60)
+            break
+
+        else:
+            print('Internet connection not available, switching to offline database...')
+
+            # setup connection
+            conn = pyodbc.connect('Driver={SQL Server};'
+                                  'Server=MININT-BDIHI4H;'
+                                  'Database=NORTHWND;'
+                                  'Trusted_Connection=yes;')
+
+            cursor = conn.cursor()
+
+            # fetching queries
+            cursor.execute('SELECT top(5) * FROM dbo.Customers')
+            for i in cursor:
+                print(i)
+            print('\n')
+
+            print('Connected to offline database.')
+            time.sleep(60)
+            cursor.close()
+            conn.close()
+
+
+# Start the script
+switch_database()
